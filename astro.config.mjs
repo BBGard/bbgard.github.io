d267f2a15f9d8cb26984c4cf7fb4fcdb711f0d56
bbgard <bgardiner@students.federation.edu.au>
2023-12-10 13:25:38 +1100

    added react components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bbgard.github.io',
-  integrations: [react()]
+  integrations: [react(), tailwind()]
 });
