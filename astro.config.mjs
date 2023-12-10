728d5f0356f2be5bb1990b08c057614dd87d547d
bbgard <bgardiner@students.federation.edu.au>
2023-12-10 13:46:31 +1100

    removed react and tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
-
-import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bbgard.github.io',
-  integrations: [react(), tailwind()]
+  integrations: []
 });
+
+// OLD
+// import { defineConfig } from 'astro/config';
+// import react from "@astrojs/react";
+// import tailwind from "@astrojs/tailwind";
+
+// export default defineConfig({
+//   site: 'https://bbgard.github.io',
+//   integrations: [react(), tailwind()]
+// });
