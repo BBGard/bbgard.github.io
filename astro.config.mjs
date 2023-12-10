01a316d4ad04235121beea88cd855c37488c4ffe
bbgard <bgardiner@students.federation.edu.au>
2023-12-10 12:14:12 +1100

    striped default theme, started homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bbgard.github.io',
+  integrations: [react()]
 });
