4f09a50526bee0e27a451bd3bd64ea8ebe1b512e
bbgard <bgardiner@students.federation.edu.au>
2023-12-12 10:41:01 +1100

    Modified about section

---
+++ src/scripts/shake.js
@@ -0,0 +1,8 @@
+console.log("loaded");
+  export default function shakeElement(element) {
+    console.log("click");
+		// element.classList.add("shake");
+		// setTimeout(() => {
+		// 	element.classList.remove("shake");
+		// }, 1000);
+	}
