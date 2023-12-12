752c96020d8c53ddd86947bbe1702327ded0af64
bbgard <bgardiner@students.federation.edu.au>
2023-12-12 15:16:15 +1100

    Refactor shakeElement function to add and remove shake class

--- src/scripts/shake.js
+++ src/scripts/shake.js
@@ -1,8 +1,8 @@
 console.log("loaded");
   export default function shakeElement(element) {
     console.log("click");
-		// element.classList.add("shake");
-		// setTimeout(() => {
-		// 	element.classList.remove("shake");
-		// }, 1000);
+		element.classList.add("shake");
+		setTimeout(() => {
+			element.classList.remove("shake");
+		}, 1000);
 	}
