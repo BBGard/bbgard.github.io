5319176fd35dfaf6f22176e24fb4344d472c0273
bbgard <gonzogardiner@gmail.com>
2024-04-29 15:10:14 +1000

    Added return to top button

--- src/scripts/returnToTop.js
+++ src/scripts/returnToTop.js
@@ -5,12 +5,23 @@ returnToTopButton.setAttribute('aria-label', 'Return to top of page');
 returnToTopButton.setAttribute('id', 'return-to-top');
 returnToTopButton.setAttribute('title', 'Return to top of page');
 
+console.log("loaded");
+console.log("retrun to top button", returnToTopButton);
+
+//print button classes
+console.log("button classes", returnToTopButton.classList);
+
 // Show the button when scrolling down
 window.addEventListener('scroll', () => {
+  console.log("scroll");
   if (window.scrollY > 100) {
-    returnToTopButton.style.display = 'block';
+    console.log("scolling down");
+    // returnToTopButton.style.display = 'block'
+    returnToTopButton.classList.remove('hidden');
+    ;
   } else {
-    returnToTopButton.style.display = 'none';
+    // returnToTopButton.style.display = 'none';
+    returnToTopButton.classList.add('hidden');
   }
 });
 
