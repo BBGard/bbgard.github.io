b5f3b498e262945a6822dd510f20c0ad5307d90f
bbgard <gonzogardiner@gmail.com>
2024-07-22 10:38:25 +1000

    temp hamburger added

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -2,7 +2,7 @@
 // Menu
 document.querySelector('.hamburger').addEventListener('click', () => {
   // Toggle the expanded class on the nav-links
-  document.querySelector('.nav-links').classList.toggle('expanded');
+  document.querySelector('.email-text').classList.toggle('expanded');
 
   // Toggle the is-active class on the hamburger
   document.querySelector('.hamburger').classList.toggle('is-active');
@@ -12,10 +12,10 @@ document.querySelector('.hamburger').addEventListener('click', () => {
 });
 
 // Add alistener to nav-links to close the menu when a link is clicked
-document.querySelectorAll('.nav-links a').forEach((link) => {
+document.querySelectorAll('.email-text a').forEach((link) => {
   link.addEventListener('click', () => {
     // Remove the expanded class from the nav-links
-    document.querySelector('.nav-links').classList.remove('expanded');
+    document.querySelector('.email-text').classList.remove('expanded');
 
     // Remove the is-active class from the hamburger
     document.querySelector('.hamburger').classList.remove('is-active');
